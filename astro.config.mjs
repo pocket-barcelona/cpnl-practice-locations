b68cb09d2c4694ef226a60e8caceecaa2d4a97f9
Darryl October <14877570+razzle-dazzle@users.noreply.github.com>
2025-06-19 16:47:36 +0200

    Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pocket-barcelona.github.io/cpnl-practice-locations',
   integrations: [react()],
+  output: "static",
   vite: {
     plugins: [tailwindcss()]
   }
